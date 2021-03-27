d08920cde42089b50a4a58f913b1e12b87f6dc8c
ysk1905 <yashsoni1905@gmail.com>
2021-03-28 04:11:01 +0530

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -74,22 +74,7 @@
 					</a>
 				</div>
 			</div>
-			<div class="col-sm-6 col-lg-3 p-b-50">
-				<h4 class="stext-301 cl0 p-b-30">
-					Newsletter
-				</h4>
-				<form>
-					<div class="wrap-input1 w-full p-b-4">
-						<input class="input1 bg-none plh1 stext-107 cl7" type="text" name="email" placeholder="email@example.com">
-						<div class="focus-input1 trans-04"></div>
-					</div>
-					<div class="p-t-18">
-						<button class="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
-							Subscribe
-						</button>
-					</div>
-				</form>
-			</div>
+			
 		</div>
 		<div class="p-t-40">
 			<div class="flex-c-m flex-w p-b-18">
