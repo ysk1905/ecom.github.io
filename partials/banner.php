952b9b19b7c45066838e5c995ed3d9ac38156de0
ysk1905 <yashsoni1905@gmail.com>
2021-03-29 18:22:30 +0530

    Update banner.php

--- partials/banner.php
+++ partials/banner.php
@@ -8,10 +8,10 @@
           <a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
             <div class="block1-txt-child1 flex-col-l">
               <span class="block1-name ltext-102 trans-04 p-b-8">
-                Women
+                Shubharambh
               </span>
               <span class="block1-info stext-102 trans-04">
-                Spring 2018
+                
               </span>
             </div>
             <div class="block1-txt-child2 p-b-4 trans-05">
@@ -29,10 +29,10 @@
           <a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
             <div class="block1-txt-child1 flex-col-l">
               <span class="block1-name ltext-102 trans-04 p-b-8">
-                Men
+                Sukoon
               </span>
               <span class="block1-info stext-102 trans-04">
-                Spring 2018
+                
               </span>
             </div>
             <div class="block1-txt-child2 p-b-4 trans-05">
@@ -45,24 +45,7 @@
       </div>
       <div class="col-md-6 col-xl-4 p-b-30 m-lr-auto">
 
-        <div class="block1 wrap-pic-w">
-          <img src="images/banner-03.jpg" alt="IMG-BANNER">
-          <a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-            <div class="block1-txt-child1 flex-col-l">
-              <span class="block1-name ltext-102 trans-04 p-b-8">
-                Accessories
-              </span>
-              <span class="block1-info stext-102 trans-04">
-                New Trend
-              </span>
-            </div>
-            <div class="block1-txt-child2 p-b-4 trans-05">
-              <div class="block1-link stext-101 cl0 trans-09">
-                Shop Now
-              </div>
-            </div>
-          </a>
-        </div>
+        
       </div>
     </div>
   </div>
