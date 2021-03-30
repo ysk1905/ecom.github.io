5b482564515ffe22aca78c2e9f6234c4e8210319
ysk1905 <yashsoni1905@gmail.com>
2021-03-30 12:09:17 +0530

    partials

--- partials/banner.php
+++ partials/banner.php
@@ -11,7 +11,7 @@
           <img src="images/banner-01.jpg" alt="IMG-BANNER">
           <a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
             <div class="block1-txt-child1 flex-col-l">
-              <span class="block1-name ltext-102 trans-04 p-b-8 .cl-white" style="color: white;">
+              <span class="block1-name ltext-102 trans-04 p-b-8 .cl-white" style="color: black;">
                 Shubharambh
               </span>
               <span class="block1-info stext-102 trans-04">
@@ -32,7 +32,7 @@
           <img src="images/banner-02.jpg" alt="IMG-BANNER">
           <a href="product.php" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
             <div class="block1-txt-child1 flex-col-l">
-              <span class="block1-name ltext-102 trans-04 p-b-8" style="color: white;">
+              <span class="block1-name ltext-102 trans-04 p-b-8" style="color: black;">
                 Sukoon
               </span>
               <span class="block1-info stext-102 trans-04">
--- partials/slider.php
+++ partials/slider.php
@@ -6,7 +6,6 @@
   }
 </style>
 
-
 <section class="section-slide">
   <div class="wrap-slick1">
     <div class="slick1">
@@ -15,11 +14,12 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
               <span class="ltext-101 cl2 respon2">
+                
               </span>
             </div>
             <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
-              <h2 class=" ltext-201 cl2 p-t-19 p-b-43 respon1 cl-white">
-                Sukoon
+              <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1" >
+                Latest Trends
               </h2>
             </div>
             <div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
@@ -35,23 +35,23 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
               <span class="ltext-101 cl2 respon2">
-
+                
               </span>
             </div>
             <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
-              <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1 cl-white">
-                Shubharambh
+              <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
+                Shop Now
               </h2>
             </div>
             <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
               <a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                Shop Now
+                Click Here
               </a>
             </div>
           </div>
         </div>
       </div>
-
+      
     </div>
   </div>
 </section>
