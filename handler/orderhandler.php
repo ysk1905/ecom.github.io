61c08191374fafbaf181c8c58812529dcc1ea464
Rishab Goyal <rishabgoyal432@gmail.com>
2021-03-30 10:12:45 +0530

    final

--- handler/orderhandler.php
+++ handler/orderhandler.php
@@ -25,8 +25,6 @@ foreach ($_SESSION['cart'] as $key => $value) {
 	$proid=$value['item_id'];
 	$quantity=$value['quantity'];
 	$size=$value['size'];
-
-
 	$sql3="INSERT Into order_details(order_id,product_id,size,quantity) VAlUES('$orderid','$proid',$size,'$quantity')";
 	$connect->query($sql3);
 	# code...
--- partials/slider.php
+++ partials/slider.php
@@ -1,3 +1,12 @@
+<style>
+  @media screen and (max-width:700px) {
+    .cl-white {
+      color: white !important;
+    }
+  }
+</style>
+
+
 <section class="section-slide">
   <div class="wrap-slick1">
     <div class="slick1">
@@ -6,11 +15,10 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
               <span class="ltext-101 cl2 respon2">
-                
               </span>
             </div>
             <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
-              <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
+              <h2 class=" ltext-201 cl2 p-t-19 p-b-43 respon1 cl-white">
                 Sukoon
               </h2>
             </div>
@@ -27,11 +35,11 @@
           <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
             <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
               <span class="ltext-101 cl2 respon2">
-                
+
               </span>
             </div>
             <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
-              <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
+              <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1 cl-white">
                 Shubharambh
               </h2>
             </div>
@@ -43,7 +51,7 @@
           </div>
         </div>
       </div>
-      
+
     </div>
   </div>
 </section>
