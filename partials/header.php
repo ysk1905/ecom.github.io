a635fefc069a1e239f1880d97f096625a208f24c
ysk1905 <yashsoni1905@gmail.com>
2021-03-31 03:59:32 +0530

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -132,9 +132,9 @@
       <!-- <li>
         <a href="blog.php">Blog</a>
       </li> -->
-      <li>
-        <a href="about.php">About</a>
-      </li>
+      <!--<li>
+      <a href="about.php">About</a>
+      </li>-->
       <li>
         <a href="contact.php">Contact</a>
       </li>
